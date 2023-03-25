bc3136181cc907704e5fb7a206eb766ed428949b
mdsorwar.mostafa <mdsorwar.mostafa@bracits.com>
2023-03-26 02:58:59 +0600

    live url added

--- app/src/main/java/com/example/facebooklite/utils/Constants.kt
+++ app/src/main/java/com/example/facebooklite/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.example.facebooklite.utils
 
 object Constants {
 
-    const val BASE_URL = "https://2dd0-103-87-214-202.in.ngrok.io"
+    const val BASE_URL = "https://facebooklitejava-production.up.railway.app/"
 }
